dcd3155ca16350e1459e78d10a0a3f64ced28072
Sarankp <47633011+Sarankp@users.noreply.github.com>
2020-12-03 22:53:56 +0530

    Update CustomerController.java

--- src/main/java/com/cg/paymentapp/controller/CustomerController.java
+++ src/main/java/com/cg/paymentapp/controller/CustomerController.java
@@ -1,11 +1,17 @@
 package com.cg.paymentapp.controller;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cg.paymentapp.beans.Customer;
@@ -26,10 +32,10 @@ public class CustomerController {
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@PostMapping("/login")
 	public ResponseEntity<Customer> validateLogin(@RequestBody Customer cust ) {
-		Customer customer=null;
+		//Customer customer=null;
 		try {
 			
-		 customer=userService.validateLogin(cust.getMobileNo(),cust.getPassword());
+		 Customer customer=userService.validateLogin(cust.getMobileNo(),cust.getPassword());
 	if(customer==null)
 	{
 		throw new InvalidInputException(null);
@@ -39,8 +45,19 @@ public class CustomerController {
 		{
 			return new ResponseEntity("Sorry! Login failed.Please enter correct credentials.",HttpStatus.NOT_FOUND);
 		}
-		return new ResponseEntity("Login Successful!", HttpStatus.OK);
-
+	//return new ResponseEntity("Login Successful!", HttpStatus.OK);
+		return new ResponseEntity(cust.getMobileNo(),HttpStatus.OK);
 	
 }
+	
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public Map<String, String> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+	    Map<String, String> errors = new HashMap<>();
+	 
+	    ex.getBindingResult().getFieldErrors().forEach(error -> 
+	        errors.put(error.getField(), error.getDefaultMessage()));
+	     
+	    return errors;
+	}
 	}
