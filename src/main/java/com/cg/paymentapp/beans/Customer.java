9c3cfa830dc2245c09b2c5b1a53a8834548c868b
Sarankp <47633011+Sarankp@users.noreply.github.com>
2020-12-03 22:52:41 +0530

    Update Customer.java

--- src/main/java/com/cg/paymentapp/beans/Customer.java
+++ src/main/java/com/cg/paymentapp/beans/Customer.java
@@ -4,69 +4,77 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
-//import jdk.jfr.Name;
-
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+@EntityScan(basePackages = {"com.cg.paymentapp.entity"})
 @Entity
-@Table(name="Logindetails_table")
-//@OnetoMany
+@Table(name="Customer_table")
 public class Customer {
-@Column	
-private String name;
-@Id
-private String mobileNo;
-@Column
-private String password;
-@OneToOne(cascade=CascadeType.ALL)
-private Wallet wallet;
-
-	public Customer() {
-	}
+	@Column	
+	private String name;
+	@Id
+	@NotEmpty(message = "Phone number is required")
+	@Pattern(regexp="^[0-9]{10}",message="Mobile number is invalid")
+	private String mobileNo; 
+	@Column
+	@NotEmpty(message = "Password is required")
+	@Pattern(regexp="^(?=.[0-9])(?=.[a-z])(?=.[A-Z])(?=.[@#$%^&-+=])(?=\\S+$).{8,20}$", message = "Invalid Password!!!")
+	private String password;
+   @OneToOne(cascade=CascadeType.ALL)
+   @JoinColumn(name="walletId")
+   private Wallet wallet;
 	
-	public Customer(String name, String mobileNum, Wallet wallet) {
-		this.name=name;
-		this.mobileNo=mobileNum;
-		this.wallet=wallet;
-}
+		public Customer() {
+		}
+		
+		public Customer(String name, String mobileNo, String password, Wallet wallet) {
+			super();
+			this.name = name;
+			this.mobileNo = mobileNo;
+			this.password = password;
+			this.wallet = wallet;
+		}
+		
+		public String getName() {
+			return name;
+		}
 	
-	public Wallet getWallet() {
-		return wallet;
-	}
+		public void setName(String name) {
+			this.name = name;
+		}
+		
+		public String getMobileNo() {
+			return mobileNo;
+		}
+		public void setMobileNo(String mobileNo) {
+			this.mobileNo = mobileNo;
+		}
+		public String getPassword() {
+			return password;
+		}
 
-	public void setWallet(Wallet wallet) {
-		this.wallet = wallet;
-	}
+		public void setPassword(String password) {
+			this.password = password;
+		}
+		public Wallet getWallet() {
+			return wallet;
+		}
 
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public String getMobileNo() {
-		return mobileNo;
-	}
-	public void setMobileNo(String mobileNo) {
-		this.mobileNo = mobileNo;
-	}
-	public String getPassword() {
-		return password;
-	}
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	@Override
-	public String toString() {
+		public void setWallet(Wallet wallet) {
+			this.wallet = wallet;
+		}
+
+		@Override
+		public String toString() {
 		return "Customer name=" + name + ", mobileNo=" + mobileNo;
-//				 + wallet;
-	}
-	
+				//+", Wallet="+wallet;
+						 
+				}
 
-	
-	
 }
