12f325b516fdae8d4df36c6235b8d61f213a5475
Sarankp <47633011+Sarankp@users.noreply.github.com>
2020-12-08 16:07:55 +0530

    Update CustomerController.java

--- src/main/java/com/cg/paymentapp/controller/CustomerController.java
+++ src/main/java/com/cg/paymentapp/controller/CustomerController.java
@@ -1,63 +1,104 @@
 package com.cg.paymentapp.controller;
-import java.util.HashMap;
-import java.util.Map;
-
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.cg.paymentapp.beans.BeneficiaryDetails;
 import com.cg.paymentapp.beans.Customer;
 import com.cg.paymentapp.exception.InvalidInputException;
-import com.cg.paymentapp.service.IUserService;
+import com.cg.paymentapp.service.IBeneficiaryService;
 
 @RestController
-@RequestMapping("/customer")
-public class CustomerController {
+@RequestMapping("/beneficiary")
+
+public class BeneficiaryController {
 	@Autowired
-	private IUserService userService;
-	@GetMapping("/hello")
-	public String sayHello() {
-		System.out.println("hello........");
-		return "Hello Cg! from Spring Framework!";
-	}
-	
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	@PostMapping("/login")
-	public ResponseEntity<Customer> validateLogin(@RequestBody Customer cust ) {
-		//Customer customer=null;
+	private IBeneficiaryService beneficiaryService;
+	//@SuppressWarnings({ "unchecked", "rawtypes" })
+	@PostMapping("/beneficiary/add")
+	public ResponseEntity<BeneficiaryDetails> addBeneficiary(@RequestBody BeneficiaryDetails bd ){
+		BeneficiaryDetails beneficiary = null;
 		try {
+			beneficiary = beneficiaryService.addBeneficiary(bd);
+			if(beneficiary== null)
+			{
+				throw new InvalidInputException(null);
+			}
+		}catch(InvalidInputException iie) {
+			return new ResponseEntity("Sorry! couldn't add!", HttpStatus.NOT_FOUND);
 			
-		 Customer customer=userService.validateLogin(cust.getMobileNo(),cust.getPassword());
-	if(customer==null)
-	{
-		throw new InvalidInputException(null);
+		}
+		return new ResponseEntity<BeneficiaryDetails>(bd,HttpStatus.OK);
 	}
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@PutMapping("/beneficiary/update")
+	public ResponseEntity<BeneficiaryDetails> updateBeneficiary(@RequestBody BeneficiaryDetails bd ){
+		BeneficiaryDetails beneficiary = null;
+		try {
+			beneficiary = beneficiaryService.updateBeneficiary(bd);
+			if(beneficiary == null)
+			{
+				throw new InvalidInputException(null);
+			}
+		}catch(InvalidInputException iie) {
+			return new ResponseEntity("Sorry! couldn't update!", HttpStatus.NOT_FOUND);
+			
 		}
-		catch(InvalidInputException e)
-		{
-			return new ResponseEntity("Sorry! Login failed.Please enter correct credentials.",HttpStatus.NOT_FOUND);
+		return new ResponseEntity<BeneficiaryDetails>(bd,HttpStatus.OK);
+		
+	}
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@DeleteMapping("/beneficiary/delete")
+public ResponseEntity<BeneficiaryDetails> deleteBeneficiary(@RequestBody BeneficiaryDetails bd ){
+		BeneficiaryDetails beneficiary ;
+		try {
+			beneficiary = beneficiaryService.deleteBeneficiary(bd);
+			if(beneficiary == null)
+			{
+				throw new InvalidInputException(null);
+			}
+		}catch(InvalidInputException iie) {
+			return new ResponseEntity("Sorry! couldn't remove!", HttpStatus.NOT_FOUND);
+			
 		}
-	//return new ResponseEntity("Login Successful!", HttpStatus.OK);
-		return new ResponseEntity(cust.getMobileNo(),HttpStatus.OK);
+		return new ResponseEntity<BeneficiaryDetails>(bd,HttpStatus.OK);
+	}
+	//@SuppressWarnings({ "rawtypes", "unchecked" })
+	@GetMapping("/beneficiary/view")
+	public ResponseEntity<BeneficiaryDetails> viewBeneficiary(@RequestBody BeneficiaryDetails bd ){
+		BeneficiaryDetails beneficiary = null;
+		try {
+			beneficiary = beneficiaryService.deleteBeneficiary(bd);
+			if(beneficiary == null)
+			{
+				throw new InvalidInputException(null);
+			}
+		}catch(InvalidInputException iie) {
+			return new ResponseEntity("Sorry! couldn't view!", HttpStatus.NOT_FOUND);
+			
+		}
+		return new ResponseEntity<BeneficiaryDetails>(bd,HttpStatus.OK);
+	}
+	@SuppressWarnings({ "unchecked", "rawtypes", "unused" })
+	@GetMapping("/beneficiary/viewAll")
 	
+	public ResponseEntity<List<BeneficiaryDetails>> viewAllBeneficiary(@RequestBody Customer customer ){
+		Customer cust = new Customer();
+		
+			List<BeneficiaryDetails>beneficiary = beneficiaryService.viewAllBeneficiary(cust);
+			if(beneficiary.isEmpty())
+			{
+				
+			return new ResponseEntity("Sorry! couldn't viewAll!", HttpStatus.NOT_FOUND);
+			
+		}
+		return new ResponseEntity<List<BeneficiaryDetails>>(beneficiary,HttpStatus.OK);
+	}
 }
-	
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public Map<String, String> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
-	    Map<String, String> errors = new HashMap<>();
-	 
-	    ex.getBindingResult().getFieldErrors().forEach(error -> 
-	        errors.put(error.getField(), error.getDefaultMessage()));
-	     
-	    return errors;
-	}
-	}
